45ddd7b3c38c38e3264fb31a29530743bd005ebf
Víctor Manuel <victormanuelap@gmail.com>
2017-04-10 16:24:52 +0200

    Borrar datos de bases de datos MySQL: Eliminar registros de una base de datos

--- Bases de datos en Python/026.py
+++ Bases de datos en Python/026.py
@@ -3,12 +3,7 @@ import pymysql
 con = pymysql.connect(user="root", password="", host="127.0.0.1", database="bdpython")
 
 cursor = con.cursor()
-cursor.execute("SELECT * FROM `example` WHERE `id`=9")
-
-rows = cursor.fetchall()
-
-for row in rows:
-    print(row)
-
+cursor.execute("DELETE FROM `example` WHERE `id`=9;")
+con.commit()
 cursor.close()
 con.close()
