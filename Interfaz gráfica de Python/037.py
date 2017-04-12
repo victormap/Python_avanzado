53ce6cb19c8ff572c91a281656bdd4732efbf211
Víctor Manuel <victormanuelap@gmail.com>
2017-04-12 21:39:05 +0200

    Utilización del constructor para una ventana: Crear y mostrar una ventana mendiante un constructor

---
+++ Interfaz gráfica de Python/037.py
@@ -0,0 +1,11 @@
+import wx
+
+class Ventana_Ejemplo(wx.Frame):
+    def __init__(self, parent, title):
+        super(Ventana_Ejemplo, self).__init__(parent, title="Segunda Ventana", size=(200, 200))
+        self.Show(True)
+
+if __name__ == '__main__':
+    app = wx.App()
+    Ventana_Ejemplo(None, title='Nueva ventana')
+    app.MainLoop()
